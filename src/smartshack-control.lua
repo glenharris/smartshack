a1af685c65a3e0d3af1c8361c47e0fa0c65baa8b
Glen Harris <glen.harris@trillium.co.nz>
2019-06-02 18:05:36 +1200

    Add extra control primitives

--- src/smartshack-control.lua
+++ src/smartshack-control.lua
@@ -2,6 +2,34 @@ local Logger = require('user.smartshack-logger')
 local Check = require('user.smartshack-check')
 local CBus = require('user.smartshack-cbus')
 
+function pulseMultipleAutoLevel(cbusGas, durationSeconds)
+  log(string.format('pulseMultipleAutoLevel %d %d', #cbusGas, durationSeconds))
+  -- Switch on, only if off or in autolevel
+  for index, cbusGa in ipairs(cbusGas) do 
+    if ( Cbus.setAutoLevelIfAutoLevel(cbusGa, 0) ) then
+      AutoTimer.pushAutoOffCbusGa(cbusGa, durationSeconds)
+    end
+  end
+end
+function cancelMultipleAutoOff(cbusGas)
+  log(string.format('cancelMultipleAutoOff %d', #cbusGas))
+  for index, cbusGa in ipairs(cbusGas) do 
+    AutoTimer.cancelAutoOffCbusGa(cbusGa)
+  end
+end
+function setMultipleAutoLevel(cbusGas)
+  log(string.format('setMultipleAutoLevel %d', #cbusGas))
+  for index, cbusGa in ipairs(cbusGas) do 
+    Cbus.setAutoLevel(cbusGa)
+  end
+end
+function setMultipleLevel(cbusGas, level)
+	log(string.format('setMultipleLevel %d %s', #cbusGas, level))
+  for index, cbusGa in ipairs(cbusGas) do 
+  	Cbus.setLevel(cbusGa, level)
+  end
+end
+
 local SceneControl = {
   logger = Logger:new('SceneControl'),
 }
@@ -56,7 +84,107 @@ function SceneControl:nextLevel2()
   SetTriggerLevel(triggerId,nextLevel)
 end  
 
+local PirPresence = {
+  logger = Logger:new('PirPresence'),
+}
+function PirPresence:new(lightCbusGas, options)
+  local object = {}   
+  setmetatable(object, self)
+  self.__index = self
+  self.lightCbusGas = lightCbusGas
+  self.durationSeconds = options.durationSeconds or 60
+  return object
+end
+function PirPresence:processPirEvent(eventValue)
+  PirPresence.logger:info('processPirEvent %d', eventValue)
+  if ( value == 0 ) then -- PIR inactive
+    pulseMultipleAutoLevel(self.lightCbusGas, self.durationSeconds)
+  else
+    cancelAutoOff(self.lightCbusGas)
+    setMultipleAutoLevel(self.lightCbusGas)
+  end
+end
+
+local DoorOpenPresence = {
+  logger = Logger:new('DoorOpenPresence'),
+}
+function DoorOpenPresence:new(lightCbusGas, options)
+  local object = {}   
+  setmetatable(object, self)
+  self.__index = self
+  self.lightCbusGas = lightCbusGas
+  self.durationSeconds = options.durationSeconds or 60
+  return object
+end
+function DoorOpenPresence:processDoorEvent(eventValue)
+  DoorOpenPresence.logger:info('processDoorEvent %d', eventValue)
+  if ( eventValue == 0 ) then -- Door closed
+    setMultipleLevel(self.lightCbusGas, 0)
+  else
+    pulseMultipleAutoLevel(self.lightCbusGas, self.durationSeconds)
+  end
+end
+
+local DoorClosedPresence = {
+  logger = Logger:new('DoorClosedPresence'),
+}
+function DoorClosedPresence:new(lightCbusGas, options)
+  local object = {}   
+  setmetatable(object, self)
+  self.__index = self
+  self.lightCbusGas = lightCbusGas
+  self.closedDurationSeconds = options.closedDurationSeconds or 300
+  self.openDurationSeconds = options.openDurationSeconds or 60
+  return object
+end
+function DoorClosedPresence:processDoorEvent(eventValue)
+  DoorClosedPresence.logger:info('processDoorEvent %d', eventValue)
+  if ( value == 0 ) then -- Door closed
+    local durationSeconds = self.closedDurationSeconds or self.openDurationSeconds
+    pulseMultipleAutoLevel(self.lightCbusGas, durationSeconds)
+  else
+    local durationSeconds = self.openDurationSeconds or self.closedDurationSeconds
+    pulseMultipleAutoLevel(self.lightCbusGas, durationSeconds)
+  end 
+end
+
+local BistableSwitch = {
+  logger = Logger:new('BistableSwitch'),
+}
+function BistableSwitch:new(triggerId, options)
+  local object = {}   
+  setmetatable(object, self)
+  self.__index = self
+  self.triggerId = triggerId
+  self.maxValue = options.maxValue
+  return object
+end
+function BistableSwitch:processSwitchEvent(eventValue)
+  BistableSwitch.logger:info('processSwitchEvent %d', eventValue)
+  local triggerLevel = GetTriggerLevel(self.triggerId)
+  if ( triggerLevel > 100 ) then
+    triggerLevel = triggerLevel - 100
+  end
+  if ( eventValue == 0 ) then
+    -- Off
+    SetTriggerLevel(self.triggerId, 0)
+    -- Remember the last value
+    SetTriggerLevel(self.triggerId, triggerLevel + 100)
+  else
+    triggerLevel = triggerLevel + 1
+    if ( triggerLevel > self.maxValue ) then
+      triggerLevel = 1
+    end
+    SetTriggerLevel(self.triggerId, triggerLevel)
+  end
+end
+
 local Control = {
   Scene = SceneControl,
+  PirPresence = PirPresence,
+  DoorOpenPresence = DoorOpenPresence,
+  DoorClosedPresence = DoorClosedPresence,
+  BistableSwitch = BistableSwitch,
 }
 return Control
+
